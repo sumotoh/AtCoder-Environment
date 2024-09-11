0f354809f56bf7264e72439ba6d1317ce1b95c8e
sumotoh <sumotoharuka@gmail.com>
2024-09-11 16:27:49 +0900

    暫定版

---
+++ workspace/templates/r/Main.R
@@ -0,0 +1,36 @@
+# Del 標準入力
+# Del 
+# Del 一列の整数(N)を受け取る。
+# Del N <- as.integer(readline())
+# Del 
+# Del 一列の文字列(S)を文字(str)として受け取る
+# Del S <- readline()
+# Del 
+# Del 一列の数字を数字として受け取る
+# Del a <- as.numeric(readline())
+# Del 
+# Del 文字(S)を数値に変換する.
+# Del as.integer(S)
+# Del 
+# Del 整数の並び(これは3つの例)をそれぞれ入力
+# Del input <- scan(what = integer(), nmax = 3, quiet = TRUE)
+# Del A <- input[1]
+# Del B <- input[2]
+# Del C <- input[3]
+# Del 
+# Del 整数の並び(D1,D2,...Dn)を配列で受け取る
+# Del D <- scan(what = integer(), quiet = TRUE)
+# Del 
+# Del 整数の縦の並び(d1,d2,...)を配列で受け取る
+# Del d <- scan(what = integer(), quiet = TRUE)
+# Del 
+# Del 標準出力
+# Del cat()
+# Del cat(ans\n)など、末尾に\nをつけるべし。
+# Del もしくは以下。
+# Del cat(answer)
+# Del cat("\n")
+# Del 
+# Del ショートカット
+# Del Alt + t : テストの実行
+# Del Alt + s Alt + u : 提出(altを押しながらs,uを押す)
